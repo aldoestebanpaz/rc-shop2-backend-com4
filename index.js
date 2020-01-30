5de8632b6a0efa0d3983e266e70f8b78de40fe9a
giomanconi <giomanconi@hotmail.com>
2020-01-29 22:43:21 -0300

    post changed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Product = require('./products')
+
 const express = require('express');
 const app = express();
 
@@ -5,13 +7,14 @@ app.use(express.json());
 
 
 //Get
-app.get('/api/products', function(req, res) {
-
+app.get('/api/products', async function(req, res) {
+  
 });
 
 //Post
 app.post('/api/products', function(req, res) {
-    
+  const doc = await Product.create(req.body);
+  res.send(doc)  
 });
 
 
